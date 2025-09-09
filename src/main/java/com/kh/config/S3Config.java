1b0f8b24546adb76dedc79ba0ad8da2a044030a4
tj <onse0607@naver.com>
2025-09-09 15:46:55 +0900

    api키 정리

--- src/main/java/com/kh/config/S3Config.java
+++ src/main/java/com/kh/config/S3Config.java
@@ -1,5 +1,6 @@
 package com.kh.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -11,37 +12,32 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class S3Config {
 
-//    @Value("${cloud.aws.credentials.access-key}")
-//    private String accessKey;
-//
-//    @Value("${cloud.aws.credentials.secret-key}")
-//    private String secretKey;
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
 
     @Bean
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
-                .region(Region.AP_NORTHEAST_2) // 서울 리전
+                .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(
-                                        System.getenv("AWS_ACCESS_KEY_ID"),
-                                        System.getenv("AWS_SECRET_ACCESS_KEY")
-                                )
-                        )
-                )
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                ))
                 .build();
     }
 
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.AP_NORTHEAST_2)
+                .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(
-                                        System.getenv("AWS_ACCESS_KEY_ID"),
-                                        System.getenv("AWS_SECRET_ACCESS_KEY")
-                                )
-                        )
-                )
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                ))
                 .build();
     }
 }
